558c1837c3c02ebc6bc3fe9021a7201d8e95ba4d
Hassan <h20.ahmadi@gmail.com>
2017-01-16 02:26:47 -0500

    3rd commit

---
+++ bin/CMakeFiles/reservation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reservation.dir/src/main.cpp.o"
+  "reservation.pdb"
+  "reservation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reservation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	    
+	return 0;
+}
+
+void save_setting()
+{
+    ofstream sav("setting.dat");
+    colour[5] = ':';
+    sav<<colour<<"\n";
+    sav<<screensav<<"\n";
+    sav<<log_out_time<<"\n";
+    sav<<screen_saver_wait_time<<"\n";
+    sav.close();
+}
+
+void load_setting()
+{
+    ifstream load("setting.dat");
+    load>>colour;
+    colour[5] = ' ';
+    load>>screensav;
+    load>>log_out_time;
+    load>>screen_saver_wait_time;
+    load.close();
+}
